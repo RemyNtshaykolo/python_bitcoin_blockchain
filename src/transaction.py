32caafd0ad7e9fadcd1595e67333a43e550c4f15
Rémy <remy.nts@gmail.com>
2021-06-02 20:42:34 +0200

    Added doc string to the functions

--- src/transaction.py
+++ src/transaction.py
@@ -1,15 +1,25 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
+from .user import User
 
 
 class Transaction:
-    def __init__(self, sender: "User", receiver: "User", amount: float):
-        self.__sender: "User" = sender
-        self.__receiver: "User" = receiver
+    def __init__(self, sender: User, receiver: User, amount: float):
+        self.__sender: User = sender
+        self.__receiver: User = receiver
         self.__amount: float = amount
         self.__signature = None
 
+    @property
+    def amount(self):
+        return self.__amount
+
+    @amount.setter
+    def amount(self, value):
+        self.__amount = value
+
     def __str__(self):
+        """Return a string representation of a transaction"""
         return "{} send {} to {}".format(
             self.__sender,
             self.__amount,
@@ -17,11 +27,12 @@ class Transaction:
         )
 
     def is_signed(self):
+        """Check if a transaction if signed. Return False if not and True otherwise"""
         return self.__signature is not None
 
     def sign(self, private_key):
         """
-        Generate the transaction signature.
+        Generate the transaction signature from a private key
         """
 
         formated_transaction = "{}_{}_{}".format(
@@ -29,9 +40,7 @@ class Transaction:
         )
         self.__signature = private_key.sign(
             formated_transaction.encode(),
-            padding.PSS(
-                mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH
-            ),
+            padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH),
             hashes.SHA256(),
         )
 
@@ -45,16 +54,6 @@ class Transaction:
         self.__sender.public_key.verify(
             self.__signature,
             formated_transaction.encode(),
-            padding.PSS(
-                mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH
-            ),
+            padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH),
             hashes.SHA256(),
         )
-
-    @property
-    def amount(self):
-        return self.__amount
-
-    @amount.setter
-    def amount(self, value):
-        self.__amount = value
