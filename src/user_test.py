3f3b9fe6ede1e6b2571f02963e61b61247092911
Rémy <remy.nts@gmail.com>
2021-06-02 21:12:15 +0200

    Remove useless test file for now

--- src/user_test.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from unittest.mock import MagicMock, patch
-from .transaction import Transaction
-from .user import User
-
-
-class UserTest(TestCase):
-    def test_encryption(self):
-        print("hello")
